c53776eb7dea32e714d7e00a179f980954c2d917
Henry Chan <hchan4155sf@gmail.com>
2019-07-11 15:57:57 -0700

    Skeleton Program

---
+++ src/Animals/Animal.java
@@ -0,0 +1,24 @@
+package Animals;
+
+public class Animal {
+	
+	public void StartSimulating()
+	{
+		
+	}
+	
+	public void Idle()
+	{
+		
+	}
+	
+	public void Walk()
+	{
+		
+	}
+	
+	public void Run()
+	{
+		
+	}
+}
---
+++ src/Park.java
@@ -0,0 +1,13 @@
+import Animals.Animal;
+
+public class Park {
+	public void RunGame()
+	{		
+		DisplayAnimals();
+	}
+	
+	public void DisplayAnimals()
+	{
+		
+	}
+}
